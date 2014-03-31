4bba06fdd355ff525f3c57d4eb1936895bb4277f
Christopher Waugh <kenkron@gmail.com>
2014-03-31 14:16:46 -0400

    Added a textureFactory to manage texture loading.  Tested in LWJGLTest.java

--- src/view/cgi/test/LWJGLTest.java
+++ src/view/cgi/test/LWJGLTest.java
@@ -1,20 +1,15 @@
 package view.cgi.test;
 
 import java.awt.Rectangle;
-import java.awt.image.BufferedImage;
-import java.io.File;
 import java.io.IOException;
-import java.nio.ByteBuffer;
-import java.util.ArrayList;
 
-import javax.imageio.ImageIO;
-
-import org.lwjgl.BufferUtils;
 import org.lwjgl.LWJGLException;
 import org.lwjgl.input.Mouse;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.DisplayMode;
 import org.lwjgl.opengl.GL11;
+
+import view.cgi.TextureFactory;
 
 public class LWJGLTest {
 
@@ -24,11 +19,12 @@ public class LWJGLTest {
 	float pitch,yaw,roll;
 	float apitch,ayaw,aroll;
 	
-	int defTexture=0;
+	int defTexture=TextureFactory.MISSING_TEXTURE;
+	int brokenTexture=TextureFactory.MISSING_TEXTURE;
 	
 	public void start() {
 		try {
-			Display.setDisplayMode(new DisplayMode(400, 400));
+			Display.setDisplayMode(new DisplayMode(800, 800));
 			Display.create();
 		} catch (LWJGLException e) {
 			e.printStackTrace();
@@ -40,13 +36,13 @@ public class LWJGLTest {
 		GL11.glEnable(GL11.GL_TEXTURE_2D);
 		
 		try {
-			loadTexture();
+			TextureFactory.loadMissingTexture("resources/Default.png");
 		} catch (IOException e1) {
-			// TODO Auto-generated catch block
 			e1.printStackTrace();
 			System.exit(-1);
 		}
-		
+		defTexture=TextureFactory.getTexture("resources/Fawkes.png");
+		brokenTexture=TextureFactory.getTexture("this.file.does.not.exist.txt");
 		
 		while (!Display.isCloseRequested()) {
 
@@ -66,13 +62,15 @@ public class LWJGLTest {
 
 			GL11.glClearColor(0.5f, 0, 0, 1);
 			GL11.glClear(GL11.GL_COLOR_BUFFER_BIT);
-			renderRectangle(new Rectangle(-5, -5, 10, 10),pitch,yaw,roll);
+			renderRectangle(defTexture,new Rectangle(-5, -5, 10, 10),pitch,yaw,roll);
 			GL11.glTranslatef(-20, 20, 0);
-			renderRectangle(new Rectangle(-5, -5, 10, 10),apitch,0,0);
+			renderRectangle(defTexture,new Rectangle(-5, -5, 10, 10),apitch,0,0);
 			GL11.glTranslatef(20, 0, 0);
-			renderRectangle(new Rectangle(-5, -5, 10, 10),0,ayaw,0);
+			renderRectangle(defTexture,new Rectangle(-5, -5, 10, 10),0,ayaw,0);
 			GL11.glTranslatef(20, 0, 0);
-			renderRectangle(new Rectangle(-5, -5, 10, 10),0,0,aroll);
+			renderRectangle(defTexture,new Rectangle(-5, -5, 10, 10),0,0,aroll);
+			GL11.glTranslatef(-20, -40, 0);
+			renderRectangle(brokenTexture,new Rectangle(-5, -5, 10, 10),apitch,ayaw,aroll);
 			
 			Display.update();
 			
@@ -88,48 +86,6 @@ public class LWJGLTest {
 		}
 
 		Display.destroy();
-	}
-	
-	public void loadTexture() throws IOException{
-		
-		//load a texture to the graphics card
-		GL11.glBindTexture(GL11.GL_TEXTURE_2D, defTexture);
-		//prepare to recieve image data from RGB integers
-		GL11.glPixelStorei(GL11.GL_UNPACK_ALIGNMENT, 1);
-		
-		//set the texture to wrap top and bottom.
-		GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_WRAP_S, GL11.GL_REPEAT);
-		GL11.glTexParameteri (GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_WRAP_T, GL11.GL_REPEAT);
-		//give it a nice linear filter (looks bettter than nearest filtering for most things)
-		GL11.glTexParameteri (GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MAG_FILTER, GL11.GL_LINEAR);
-		GL11.glTexParameteri (GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MIN_FILTER, GL11.GL_LINEAR);
-		
-		//modulate allows for shading
-		GL11.glTexEnvf(GL11.GL_TEXTURE_ENV, GL11.GL_TEXTURE_ENV_MODE, GL11.GL_MODULATE);
-		
-		BufferedImage image = ImageIO.read(new File("resources/Fawkes.png"));
-		
-		int[] pixels = new int[image.getWidth() * image.getHeight()];
-		image.getRGB(0, 0, image.getWidth(), image.getHeight(), pixels, 0,
-				image.getWidth());
-
-		ByteBuffer buffer = BufferUtils.createByteBuffer(image.getWidth()
-				* image.getHeight() * RGB); // 4 for RGBA, 3 for RGB
-
-		for (int y = 0; y < image.getHeight(); y++) {
-			for (int x = 0; x < image.getWidth(); x++) {
-				int pixel = pixels[y * image.getWidth() + x];
-				buffer.put((byte) ((pixel >> 16) & 0xFF)); // Red component
-				buffer.put((byte) ((pixel >> 8) & 0xFF)); // Green component
-				buffer.put((byte) (pixel & 0xFF)); // Blue component
-				//buffer.put((byte) ((pixel >> 24) & 0xFF)); // Alpha component.
-															// Only for RGBA
-			}
-		}
-		
-		buffer.flip(); // FOR THE LOVE OF GOD DO NOT FORGET THIS
-		
-		GL11.glTexImage2D(GL11.GL_TEXTURE_2D, 0, GL11.GL_RGB, image.getWidth(), image.getHeight(), 0, GL11.GL_RGB, GL11.GL_UNSIGNED_BYTE, buffer);
 	}
 
 	
@@ -152,9 +108,11 @@ public class LWJGLTest {
 		}
 	}
 	
+	
+	
 	/**renders a rectangle who'se front is understood to be in positive x
 	 * Pitch, yaw, and roll are all in degreess*/
-	public void renderRectangle(Rectangle r, float pitch, float yaw, float roll){
+	public void renderRectangle(int texture, Rectangle r, float pitch, float yaw, float roll){
 		//you want to translate this object, but not the rest of the world,
 		//so you push a new transformation matrix for this object, 
 		//then pop it when you're done.
@@ -168,9 +126,9 @@ public class LWJGLTest {
 		GL11.glRotatef(yaw, 0, 1, 0);
 		GL11.glRotatef(pitch, 0, 0, 1);
 		GL11.glRotatef(roll, 1, 0, 0);
-
+		
 		GL11.glColor3f(1, 1, 1);
-		GL11.glBindTexture(GL11.GL_TEXTURE_2D, defTexture);
+		GL11.glBindTexture(GL11.GL_TEXTURE_2D, texture);
 		GL11.glBegin(GL11.GL_QUADS);
 		
 		
