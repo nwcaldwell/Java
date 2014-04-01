4a0500363a511c0e4f654c47e04a7535bc02c6f6
Christopher Waugh <kenkron@gmail.com>
2014-04-01 10:14:04 -0400

    adding method stubs to HexTile and HexSpace.  Still not implemented, but won't give compile errors.

--- src/board/HexTile.java
+++ src/board/HexTile.java
@@ -2,4 +2,16 @@ package board;
 
 public class HexTile extends Tile<HexTile> {
 
+	@Override
+	public HexTile getAdjacent(int direction) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void rotate() {
+		// TODO Auto-generated method stub
+		
+	}
+
 }
