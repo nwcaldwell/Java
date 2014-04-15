7b06ec15cd86234686d59f0e33e6d98fdf94fbf0
Christopher Waugh <kenkron@gmail.com>
2014-04-15 16:08:40 -0400

    updating boardview skeleton.

--- src/view/controls/BoardView.java
+++ src/view/controls/BoardView.java
@@ -1,16 +1,30 @@
 package view.controls;
 
+import java.util.ArrayList;
+
 import view.MediaController;
+import view.ViewController;
 
 import javax.swing.*;
 
+import models.board.HexDirection;
+
 //TODO [Sydney Christopher] [Jorge]
 
-public class BoardView extends JPanel {
+public abstract class BoardView extends JPanel {
     private MediaController mediaC;
 
-    public BoardView(MediaController media){
+    public BoardView(ViewController vc, MediaController media){
         this.mediaC = media;
 
     }
+    
+    /**redraws the entire board*/
+    public abstract void update();
+    
+    /**draws a hilighted space*/
+    public abstract void hilightSpace(ArrayList<HexDirection> dir, int height);
+    
+    /**draws a developer*/
+    public abstract void displayDev(ArrayList<HexDirection> dir);
 }
