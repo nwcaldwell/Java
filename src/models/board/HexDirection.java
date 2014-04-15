71b28e1f709a3a2bf955d458ba7dff35b97f504f
William Macfarlane <wmacfarlane@ufl.edu>
2014-04-15 16:14:52 -0400

    Directions implemented

--- src/models/board/HexDirection.java
+++ src/models/board/HexDirection.java
@@ -1,5 +1,10 @@
 package models.board;
 
-public enum HexDirection implements Direction{
-	
+public enum HexDirection implements Direction {
+	N,NE,SE,S,SW,NW;
+
+	@Override
+	public HexDirection rotate() {
+		return values()[(this.ordinal() + 1) % this.values().length];
+	}
 }
