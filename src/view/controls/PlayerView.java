8d4f26a9f42fb3689bc77e83b8742890f2306bd5
jorgep19 <jorgelpaez19@gmail.com>
2014-04-15 16:17:01 -0400

    added code for full screen

--- src/view/controls/PlayerView.java
+++ src/view/controls/PlayerView.java
@@ -3,7 +3,7 @@ package view.controls;
 import javax.swing.*;
 
 public class PlayerView extends JPanel{
-<<<<<<< HEAD
+
     private JLabel actionPoints, famePoints, developers, twoTiles, riceTiles, villageTiles, actionTokens, palaceCards;
 
     public PlayerView(){
@@ -41,10 +41,4 @@ public class PlayerView extends JPanel{
     public void setNumPalaceCards(int num){
         palaceCards.setText(""+num);
     }
-=======
-
-    public PlayerView() {
-
-    }
->>>>>>> master
 }
