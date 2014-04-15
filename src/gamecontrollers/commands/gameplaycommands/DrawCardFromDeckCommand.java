ba88651df134428bc1c1b33fd525368151faf266
Sydney Richardson <sydneyrichardson3@gmail.com>
2014-04-15 13:14:44 -0400

    updated DrawCardFromDeckCommand

--- src/gamecontrollers/commands/gameplaycommands/DrawCardFromDeckCommand.java
+++ src/gamecontrollers/commands/gameplaycommands/DrawCardFromDeckCommand.java
@@ -1,7 +1,8 @@
 // TODO developer [ Jorge ], test [ Sydney ]
-package view.commands.gameplaycommands;
+package gamecontrollers.commands.gameplaycommands;
 
 import gamecontrollers.commands.GameplayActionCommand;
+import gamecontrollers.save.CommandSaveVisitor;
 import models.palacefestival.JavaPlayer;
 import models.palacefestival.PalaceCard;
 
@@ -9,7 +10,13 @@ public class DrawCardFromDeckCommand implements GameplayActionCommand {
     private JavaPlayer player;
     private PalaceCard card;
 
-	@Override	public void execute() {
-		throw new UnsupportedOperationException();
-	}
+    @Override	public void execute() {
+        throw new UnsupportedOperationException();
+    }
+    @Override	public void undo() {
+        throw new UnsupportedOperationException();
+    }
+    @Override	public void accept(CommandSaveVisitor visitor) {
+        throw new UnsupportedOperationException();
+    }
 }
