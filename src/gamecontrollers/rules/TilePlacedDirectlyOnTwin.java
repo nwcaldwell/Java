d29f2d0859dc1c3294887c122ded9d2ed5267983
William Macfarlane <wmacfarlane@ufl.edu>
2014-04-15 00:36:48 -0400

    Added lots of rules

---
+++ src/gamecontrollers/rules/TilePlacedDirectlyOnTwin.java
@@ -0,0 +1,17 @@
+package gamecontrollers.rules;
+
+/**
+ * Created by williammacfarlane on 4/15/14.
+ */
+public class TilePlacedDirectlyOnTwin extends TilePlacementRule{
+
+	@Override
+	public void update() {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public String getErrorMessage() {
+		throw new UnsupportedOperationException();
+	}
+}
