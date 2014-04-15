e5d7176dc7c1cc4cf48ae05104835474d83e5a4c
jorgep19 <jorgelpaez19@gmail.com>
2014-04-14 21:25:18 -0400

    solved palace festival package naming issue

---
+++ src/models/palacefestival/PalaceCard.java
@@ -0,0 +1,25 @@
+package models.palacefestival;
+
+import java.util.ArrayList;
+
+public class PalaceCard {
+    private ArrayList<Symbol> symbols;
+	
+	public PalaceCard(Symbol symbol){
+        symbols = new ArrayList<Symbol>();
+        symbols.add(symbol);
+	}
+
+    public PalaceCard(Symbol sym1, Symbol sym2){
+        symbols = new ArrayList<Symbol>();
+        symbols.add(sym1);
+        symbols.add(sym2);
+    }
+
+    //returns the number of symbols that the cards have in common
+    public int compare(PalaceCard card){
+        int points = 0;
+
+        return points;
+    }
+}
