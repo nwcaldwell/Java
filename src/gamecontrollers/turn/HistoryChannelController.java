9cc68cb1497297e60ff9aa44955a861fc8cd1575
Sydney Richardson <sydneyrichardson3@gmail.com>
2014-04-14 23:47:55 -0400

    created Planning, Replay, and History controllers

---
+++ src/gamecontrollers/turn/HistoryChannelController.java
@@ -0,0 +1,25 @@
+package gamecontrollers.turn;
+
+
+import models.board.Player;
+
+public class HistoryChannelController {
+    private Stack<GameplayActionCommand> commandHistory;
+    private List<GameplayActionCommand> lastRound;
+
+    public HistoryChannelController(){
+
+    }
+
+    public void addCommand(GameplayActionCommand c){
+
+    }
+
+    public Stack<GameplayActionCommand> getGameHistory(){
+
+    }
+
+    public List<GameplayActionCommand> getLastRound(Player p){
+
+    }
+}
---
+++ src/gamecontrollers/turn/ReplayController.java
@@ -0,0 +1,26 @@
+package gamecontrollers.turn;
+
+
+public class ReplayController {
+    private List<GameplayActionCommand> replayList;
+
+    public ReplayController(){
+
+    }
+
+    public void goForward(){
+
+    }
+
+    public void goBackward(){
+
+    }
+
+    public void goAll(){
+
+    }
+
+    public void setReplayList(List<GameplayActionCommand>){
+
+    }
+}
