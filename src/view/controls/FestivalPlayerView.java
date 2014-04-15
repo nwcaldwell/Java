d2bd968e1f30158b497288f9859f3c02c0d2f0b4
Sydney Richardson <sydneyrichardson3@gmail.com>
2014-04-14 22:13:54 -0400

    updated Festival Views

--- src/view/controls/FestivalPlayerView.java
+++ src/view/controls/FestivalPlayerView.java
@@ -5,6 +5,9 @@ import javax.swing.*;
 //TODO [Sydney][Jorge]
 
 public class FestivalPlayerView extends JPanel {
+    private JLabel currentBid;
+    private JLabel[] palaceCards;
+    private JButton dropOut;
 
     public FestivalPlayerView(){
 
--- src/view/screens/gameplay/FestivalView.java
+++ src/view/screens/gameplay/FestivalView.java
@@ -3,6 +3,7 @@ package view.screens.gameplay;
 import view.*;
 import view.controls.FestivalPlayerView;
 
+import javax.swing.*;
 import java.util.List;
 
 //TODO [Sydney][Jorge]
@@ -10,6 +11,8 @@ import java.util.List;
 public class FestivalView extends View {
 
     private List<FestivalPlayerView> playersList;
+    private JLabel festivalCard;
+    private JLabel highestBid;
 
     public FestivalView(ViewController viewC, MediaController mediaC){
         super(viewC, mediaC);
