9d23373cc42dd1b572a142c1ab3241d7e8116ec3
Sydney Richardson <sydneyrichardson3@gmail.com>
2014-04-15 13:09:49 -0400

    adding more skeleton code to the commands

--- src/gamecontrollers/commands/gameplaycommands/DrawCardFromDeckCommand.java
+++ src/gamecontrollers/commands/gameplaycommands/DrawCardFromDeckCommand.java
@@ -2,8 +2,12 @@
 package view.commands.gameplaycommands;
 
 import gamecontrollers.commands.GameplayActionCommand;
+import models.palacefestival.JavaPlayer;
+import models.palacefestival.PalaceCard;
 
 public class DrawCardFromDeckCommand implements GameplayActionCommand {
+    private JavaPlayer player;
+    private PalaceCard card;
 
 	@Override	public void execute() {
 		throw new UnsupportedOperationException();
--- src/gamecontrollers/commands/gameplaycommands/DrawFestivalCardCommand.java
+++ src/gamecontrollers/commands/gameplaycommands/DrawFestivalCardCommand.java
@@ -2,8 +2,12 @@
 package view.commands.gameplaycommands;
 
 import gamecontrollers.commands.GameplayActionCommand;
+import models.palacefestival.JavaPlayer;
+import models.palacefestival.PalaceCard;
 
 public class DrawFestivalCardCommand implements GameplayActionCommand {
+    private JavaPlayer player;
+    private PalaceCard festivalCard;
 
 	@Override	public void execute() {
 		throw new UnsupportedOperationException();
