b4454137972eba15330d8ebc4584a6b27495722d
William Macfarlane <wmacfarlane@ufl.edu>
2014-04-18 00:36:58 -0400

    Board Cosntruction Crew's been put to work

---
+++ src/models/board/BoardConstructionCrew.java
@@ -0,0 +1,11 @@
+package models.board;
+
+
+import java.io.IOException;
+
+/**
+ * Created by williammacfarlane on 4/17/14.
+ */
+public abstract class BoardConstructionCrew {
+	public abstract Space buildBoard(String file);
+}
