df1a24842a4af240e39497a3dd5d368d77d3892b
jorgep19 <jorgelpaez19@gmail.com>
2014-04-19 21:23:02 -0400

    Implemented pair class

--- src/view/commands/gameplayInput/GameplayInputCommand.java
+++ src/view/commands/gameplayInput/GameplayInputCommand.java
@@ -1,11 +1,15 @@
 package view.commands.gameplayInput;
 
 import view.commands.InputCommand;
+import view.screens.gameplay.GameplayView;
 
+// TODO verify is need
 public abstract class GameplayInputCommand implements InputCommand {
+
+    private GameplayView view;
 
     @Override
     public void execute() {
-
+        view.update();
     }
 }
