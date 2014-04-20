9d2b6c933837e549ac7694d92f59962c4ef21ac4
ktn <kevinnieman@gmail.com>
2014-04-19 21:25:06 -0400

    Fix the functionality for planning mode and saving of commands

--- src/gamecontrollers/turn/PlanningModeCommandHandler.java
+++ src/gamecontrollers/turn/PlanningModeCommandHandler.java
@@ -2,29 +2,45 @@ package gamecontrollers.turn;
 
 //TODO [Kevin][Will]
 import gamecontrollers.commands.GameplayActionCommand;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
 import java.util.Stack;
 
 public class PlanningModeCommandHandler implements CommandHandler {
-    private Stack<GameplayActionCommand> plannedMoves;
+    private List<GameplayActionCommand> plannedMoves;
+    private HistoryChannelController history;
 
-
-    private PlanningModeCommandHandler(){
-
+    private PlanningModeCommandHandler(HistoryChannelController history){
+        plannedMoves = new ArrayList<GameplayActionCommand>();
+        this.history = history;
     }
 
+    //command has been planned, add to list of planned commands
     public void handleCommand(GameplayActionCommand c){
-        plannedMoves.push(c);
+        plannedMoves.add(c);
     }
 
-    public Stack<GameplayActionCommand> getPlannedMoves(){
+    public List<GameplayActionCommand> getPlannedMoves(){
         return plannedMoves;
     }
 
+    //remove the last command that was planned
     public void cancelCommand(){
-        plannedMoves.pop();
+        plannedMoves.remove(plannedMoves.size()-1);
     }
 
+    //remove the list of planned commands
     public void clearPlannedCommands(){
-        plannedMoves.removeAllElements();
+        plannedMoves.clear();
+    }
+
+    //execute and save all planned commmands
+    public void executePlannedCommands(){
+        for(GameplayActionCommand command :plannedMoves){
+            command.execute();
+            history.addCommand(command);
+        }
     }
 }
