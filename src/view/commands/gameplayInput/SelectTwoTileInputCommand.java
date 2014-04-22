29499d2d6dd9e7f35144cb4b2eacc2a587780ec0
ktn <kevinnieman@gmail.com>
2014-04-21 21:27:49 -0400

    Fix error

--- src/view/commands/gameplayInput/SelectTwoTileInputCommand.java
+++ src/view/commands/gameplayInput/SelectTwoTileInputCommand.java
@@ -29,6 +29,6 @@ public class SelectTwoTileInputCommand extends InputCommand {
     @Override	public void execute() {
         Facade.getInstance().startPlacingTile( new VR( ).buildTile(HexDirection.N) );
         getViewController().removeCurrentKeyListeners();
-        getViewController().resetKeyActionListeners(keyListeners);
+        getViewController().addKeyListeners(keyListeners);
 	}
 }
